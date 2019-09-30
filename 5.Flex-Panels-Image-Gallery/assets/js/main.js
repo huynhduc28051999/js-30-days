e39bf16ff13e7d5e637d8d602333431ce484ec71
huynhduc280519999 <huynhduc28051999@gmail.com>
2019-09-30 20:44:29 +0700

    flex panel image gallery

---
+++ 5.Flex-Panels-Image-Gallery/assets/js/main.js
@@ -0,0 +1,14 @@
+let panels = document.querySelectorAll('.panel');
+
+function toggleOpen() {
+  this.classList.toggle('open');
+}
+function toggleActive(e) {
+  // console.log(e.propertyName);
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('active');
+  }
+
+}
+panels.forEach(panel => panel.addEventListener('click', toggleOpen));
+panels.forEach(panel => panel.addEventListener('transitionend', toggleActive));
