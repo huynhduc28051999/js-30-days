d9f8c0d2f71d297b4eef448407c5db4e2b863a66
huynhduc280519999 <huynhduc28051999@gmail.com>
2019-10-03 16:09:26 +0700

    day 4 session 22

---
+++ 22.Follow-Along-Links/assets/js/main.js
@@ -0,0 +1,19 @@
+let triggers = document.querySelectorAll('a');
+let highlight = document.createElement('span');
+highlight.classList.add('highlight');
+document.body.appendChild(highlight);
+function highlightLink() {
+  // console.log(this);
+  let linkCoords = this.getBoundingClientRect();
+  // console.log(linkCoords);
+  let coords = {
+    width: linkCoords.width,
+    height: linkCoords.height,
+    top: linkCoords.top + window.scrollY,
+    left: linkCoords.left + window.scrollX
+  }
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+}
+triggers.forEach(a => a.addEventListener('mouseenter',highlightLink));
