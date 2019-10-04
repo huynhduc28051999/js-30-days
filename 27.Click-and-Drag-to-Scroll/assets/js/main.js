fd30ab0efc70bb0b455d7d1beaaceedbd008e78d
huynhduc280519999 <huynhduc28051999@gmail.com>
2019-10-04 10:34:12 +0700

    day5, session 27

---
+++ 27.Click-and-Drag-to-Scroll/assets/js/main.js
@@ -0,0 +1,32 @@
+const slider = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+console.log(slider);
+
+
+slider.addEventListener('mousedown',(e) =>{
+  isDown =true;
+  slider.classList.add('active');
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener('mouseleave',() =>{
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mouseup',() =>{
+  isDown = false;
+  slider.classList.remove('active');
+});
+slider.addEventListener('mousemove',(e) =>{
+  if(!isDown) return;
+  // console.log('ok');
+  // console.log(startX);
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  console.log(x,startX);
+  const walk = (x - startX) * 3;
+  slider.scrollLeft = scrollLeft - walk;
+  
+});
